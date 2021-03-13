6e292f16ddafeb28db6d06fceb78533bc220ef2a
吉田　喜昭 <>
2021-03-13 18:46:05 +0900

    スクレイピングで為替取得するよう修正

--- api/utils/scraping.rb
+++ api/utils/scraping.rb
@@ -1,1 +1,14 @@
-print 108.55
+require 'net/http'
+require 'nokogiri'
+
+url = "https://info.finance.yahoo.co.jp/fx/detail/?code=usdjpy"
+uri = URI(url)
+html = Net::HTTP.get(uri)
+
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+# puts doc
+low_nodes = doc.css('dd#USDJPY_detail_low').text.to_f
+heigh_nodes = doc.css('dd#USDJPY_detail_high').text.to_f
+print ((low_nodes + heigh_nodes)/2).round(4)
+
+
